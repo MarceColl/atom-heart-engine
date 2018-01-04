477cf6de694c2c96e6adce83d6fa6e22fa63b15b
Marce Coll <marce@marcecoll.com>
2018-01-04 02:04:44 +0100

    Split Tupfile properly, now the library doesn't link to glfw and dl as it doesn't need it.

--- src/game.cpp
+++ src/game.cpp
@@ -3,9 +3,11 @@
 #include <game.hpp>
 
 extern "C" GAME_UPDATE_FUNC(game_update) {
+	// TODO(Marce): Call all the systems update functions
 	printf("Testing zee super updates\n");
 }
 
 extern "C" GAME_RENDER_FUNC(game_render) {
+	// TODO(Marce): Do the rendering
 	printf("Testing render\n");
 }
